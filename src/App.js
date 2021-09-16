6acdffd43f7f6545f6c23bd38c290101f81ec2ca
NIKHIL KUMAR <nikhilkr2396@gmail.com>
2021-09-16 12:52:47 +0530

    frontend : Created components(Navbar,Home,About) and used router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import { About } from './Components/About';
+import { Home } from './Components/Home';
+import Navbar from './Components/Navbar'
 
-App = () => {
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <><Router>
+        <Navbar/>
+                
+        <Switch>
+            
+            <Route exact path="/">
+              <Home />
+            </Route>
+            
+            <Route exact path="/about">
+              <About />
+            </Route>
+            
+        </Switch>
+      
+      </Router>
+    </>
   );
 }
 
