0db635350997ed42ba404658c4acffb40e12264a
NIKHIL KUMAR <nikhilkr2396@gmail.com>
2021-09-16 19:47:56 +0530

    frontend : used useLocation hook and created partial Home page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import { About } from './Components/About';
-import { Home } from './Components/Home';
-import Navbar from './Components/Navbar'
-import NoteState from './Context/notes/NoteState';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { About } from "./Components/About";
+import { Home } from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import NoteState from "./Context/notes/NoteState";
 
 function App() {
   return (
     <>
-    <NoteState>
-      <Router>
-        <Navbar/>
-                
-        <Switch>
-            
-            <Route exact path="/">
-              <Home />
-            </Route>
-            
-            <Route exact path="/about">
-              <About />
-            </Route>
-            
-        </Switch>
-      
-      </Router>
-    </NoteState>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </NoteState>
     </>
   );
 }
