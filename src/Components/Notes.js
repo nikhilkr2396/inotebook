3c470674d654e756d0b4384e524c6587fb077a6b
NIKHIL KUMAR <nikhilkr2396@gmail.com>
2021-09-28 00:26:02 +0530

    frontend : Adding AddNote component

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,20 +1,23 @@
-import React,{useContext} from 'react'
-import noteContext from "../Context/notes/noteContext"
-import Noteitem from './Noteitem'
+import React, { useContext } from "react";
+import noteContext from "../Context/notes/noteContext";
+import AddNote from "./AddNote";
+import Noteitem from "./Noteitem";
 
 function Notes() {
-    
-    const context = useContext(noteContext);
-    const {notes, setNotes} = context;
+  const context = useContext(noteContext);
+  const { notes, addnote } = context;
 
-    return (
-        <div className="row my-3">
-            <h2>Your Notes</h2>
-                {notes.map((note) =>{
-                    return <Noteitem key={note.id} note={note}/>;
-            })}
-        </div>
-    )
+  return (
+    <>
+      <AddNote/>
+      <div className="row my-3">
+        <h2>Your Notes</h2>
+        {notes.map((note) => {
+          return <Noteitem key={note._id} note={note} />;
+        })}
+      </div>
+    </>
+  );
 }
 
-export default Notes
+export default Notes;
--- src/Context/notes/NoteState.js
+++ src/Context/notes/NoteState.js
@@ -5,7 +5,7 @@ const NoteState = (props) => {
 
     const initialNotes = [
         {
-          "_id": "6140e440dca88fbc32144e65",
+          "_id": "6140e440dcaq88fbc3bv144e65",
           "user": "61372becdd579253cdb95fee",
           "title": "First Note",
           "description": "This is the first note. The app is in the production phase.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose",
@@ -14,7 +14,7 @@ const NoteState = (props) => {
           "__v": 0
         },
         {
-          "_id": "6140e440dca88fbc32144e67",
+          "_id": "6140e440dca88qfbc32vd144e67",
           "user": "61372becdd579253cdb95fee",
           "title": "First Note",
           "description": "This is the first note. The app is in the production phase.The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'",
@@ -23,7 +23,7 @@ const NoteState = (props) => {
           "__v": 0
         },
         {
-          "_id": "6140e440dca88fbc32144e69",
+          "_id": "6vd140e440dca88fbc32144e69",
           "user": "61372becdd579253cdb95fee",
           "title": "First Note",
           "description": "This is the first note. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum'",
@@ -32,7 +32,7 @@ const NoteState = (props) => {
           "__v": 0
         },
         {
-          "_id": "6140e440dca88fbc32144e69",
+          "_id": "6140e440dca88fbc321wd44se69",
           "user": "61372becdd579253cdb95fee",
           "title": "Second Note",
           "description": "This is the first note. The app is in the production phase.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here",
@@ -41,7 +41,7 @@ const NoteState = (props) => {
           "__v": 0
         },
         {
-          "_id": "6140e441dca88fbc32144e6b",
+          "_id": "6140e441dcsda88fbc32144e6b",
           "user": "61372becdd579253cdb95fee",
           "title": "First Note",
           "description": "This is the first note. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose.",
@@ -53,8 +53,33 @@ const NoteState = (props) => {
 
     const [notes , setNotes] = useState(initialNotes);
 
+    // Add a Note
+      const addnote = (title, description, tag) => {
+        // TODO: API calls
+        const note = {
+          "_id": "6140e441dcsda88fbc32aw144e62b",
+          "user": "61372becdd579253cdb95fee4",
+          "title": title,
+          "description": description,
+          "tags": tag,
+          "date": "2021-09-14T18:04:49.067Z",
+          "__v": 0
+        };
+        setNotes(notes.concat(note))
+      }
+
+    // Deleete a Note
+      const deletenote = () => {
+
+      }
+
+    //  Edit a Note
+      const editnote = () => {
+
+      }
+
     return(
-        <NoteContext.Provider value={{notes , setNotes}}>
+        <NoteContext.Provider value={{notes, addnote, deletenote, editnote}}>
             {props.children}
         </NoteContext.Provider>
     )
